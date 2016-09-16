2a554587d8b512ae600caff357562494a1b0b6f8
Robin1711 <robin.sommer@planet.nl>
2016-09-16 17:12:20 +0200

    problem 4: putInList misses the 0s

---
+++ fpProblem4.hs
@@ -0,0 +1,35 @@
+--lastDigits :: Int -> Int -> [Int]
+--lastDigits n d
+--	| digits s < digits d = putInList s
+--	| otherwise = putInList (getCorrectDigitsOfSum s)
+--	where s = sumNum n n 0
+
+sumNum :: Int -> Int -> Int -> Int
+sumNum num pow res
+	| pow < 0  = res
+	| otherwise = sumNum num (pow-1) (res + num^pow)
+
+--putInList :: Int -> [Int]
+--putInList s
+--	| mod s 10 == s = [s]
+--	| removeFirstDigit s == div s 100 = [getFirstDigit s] ++ [0] ++ putInList (removeFirstDigit s)
+--	| otherwise = [getFirstDigit s] ++ putInList (removeFirstDigit s)
+
+
+getCorrectDigitsOfSum :: Int -> Int -> Int
+getCorrectDigitsOfSum d num
+	| digits num == d = num
+	| otherwise = getCorrectDigitsOfSum d (removeFirstDigit num)
+
+digits :: Int -> Int
+digits n
+	| mod n 10 == n = 1
+	| otherwise = 1 + digits (div n 10)
+
+removeFirstDigit :: Int -> Int
+removeFirstDigit num = num - ((getFirstDigit num ) * (10^((digits num) -1)))
+
+getFirstDigit :: Int -> Int
+getFirstDigit n
+	| mod n 10 == n = n
+	| otherwise = getFirstDigit (div n 10)
