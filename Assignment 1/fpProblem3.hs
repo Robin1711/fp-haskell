dad2ac5723833cf944109f0188ffe3adfd801023
Robin1711 <robin.sommer@planet.nl>
2016-09-26 15:41:34 +0200

    problem 3 completed

--- Assignment 1/fpProblem3.hs
+++ Assignment 1/fpProblem3.hs
@@ -1,44 +1,46 @@
 rpnEval:: String -> Int
-rpnEval s = calc (snd (getNumber s 0)) (fst (getNumber s 0)) 0
+rpnEval s = calc s []
 
-calc :: String -> Int -> Int -> Int
-calc s i j
-	| (s == "")	= i
-	| getCharacter s == -5 = calc (tail s) i j
-	| getCharacter s < 0 = calc (tail s) (useOp i j (getCharacter s)) 0
-	| getCharacter s >= 0 && getCharacter (tail s) >= 0 = calc (snd (getNumber s 0)) i (fst (getNumber s 0)) 
-	| otherwise = calc (tail s) i (getCharacter s)
+calc :: String -> [Int] -> Int
+calc [] stack = head stack
+calc s stack
+	| head s == ' '				= calc (tail s) stack
+	| getCharacter (head s) < 0 = calc (tail s) ((useOp (head bottom) (head stack) (getCharacter (head s))) : (tail bottom))
+	| otherwise					= calc (snd(getNumber s 0)) ((fst(getNumber s 0)) : stack)
+	where bottom = tail stack
 
 getNumber :: String -> Int -> (Int, String)
-getNumber s i
-	| not(s == "") && getCharacter s >= 0 = getNumber (tail s) (i*10 + getCharacter s)
-	| otherwise 	= (i, s)
+getNumber [] i = (i,[])
+getNumber (x:xs) i
+	| getCharacter x >= 0	= getNumber xs (i*10 + getCharacter x)
+	| otherwise				= (i, s)
+	where s = [x]++xs
+	
+getCharacter :: Char -> Int
+getCharacter x
+	| charIsNum x 		= charToNum x
+	| charIsOperator x	= charToOperator x
+	| otherwise			= -5
 
 useOp :: Int -> Int -> Int -> Int
 useOp i j o
-	| o == -1 = i + j
-	| o == -2 = i - j
-	| o == -3 = i * j
+	| o == -1 			= i + j
+	| o == -2			= i - j
+	| o == -3 			= i * j
 	| o == -4 && j /= 0 = div i j
 	| otherwise = error "Division by zero" -- if the divisor j is 0 we return ????
-	
-getCharacter :: String -> Int
-getCharacter s
-	| charIsNum (head s) = charToNum (head s)
-	| charIsOperator (head s) = charToOperator (head s)
-	| otherwise = -5
 
 charIsNum:: Char -> Bool
 charIsNum ch
-	|('0'<=ch)&&(ch<='9')	= True
-	| otherwise				= False
+	|(ch >='0') && (ch <= '9')	= True
+	| otherwise					= False
 	
 charIsOperator:: Char -> Bool
 charIsOperator ch
-	|ch == '+'	= True
-	|ch == '-'	= True
-	|ch == '*'	= True
-	|ch == '/'	= True
+	| ch == '+'	= True
+	| ch == '-'	= True
+	| ch == '*'	= True
+	| ch == '/'	= True
 	| otherwise	= False
 
 charToNum:: Char -> Int
@@ -46,9 +48,9 @@ charToNum ch = fromEnum ch - 48
 	
 charToOperator:: Char -> Int
 charToOperator ch
-	|ch == '+'	= -1
-	|ch == '-'	= -2
-	|ch == '*'	= -3
-	|ch == '/'	= -4
+	| ch == '+'	= -1
+	| ch == '-'	= -2
+	| ch == '*'	= -3
+	| ch == '/'	= -4
 
 main =  print . rpnEval =<< getLine
